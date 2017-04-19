d01b825d814fb9f434734adcbd74ba5eae0d9d15
palmodes <alexandre.defranc@gmail.com>
2017-04-19 08:56:56 +0200

    Add files via upload

---
+++ p2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<body>
+	<?php 
+ $clients = array (
+	"Luc", 
+	7 => "Paul", 
+	2 =>"Martin", 
+	"Arnaud"
+	);
+    print_r($clients);
+    $produits = array (
+	20 => "Chemise",
+	3 => "Pantalon",
+	10 => "Jupe",
+	"Veste",
+	"Blouson"
+	);
+    print_r($produits)
+    ?>
+	</body>
+</html>
